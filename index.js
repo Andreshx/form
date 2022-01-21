2566e8000628ee448e657fbc9ee0e83221d453c9
Andres <andreshx1996-e@hotmail.com>
2022-01-21 12:04:49 -0500

    Se agrego una estructura

---
+++ index.js
@@ -0,0 +1,9 @@
+const  suma = (n , n1,n2 )=>{ 
+    let res ; 
+    res = n+n1+n2 ; 
+    return res
+    
+    }
+    
+    console.log("Sistema de Pruebas Modifique este String" + res );
+    console.log(res);
